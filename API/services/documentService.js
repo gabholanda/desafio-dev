9ca7c692d80b83d2908ff5622fce1826d4c643f4
Gabriel Holanda <gabriel_ebot@hotmail.com>
2022-03-08 23:07:21 -0300

    Added new endpoints to documentService and updated docker-compose file to support frontend project

--- API/services/documentService.js
+++ API/services/documentService.js
@@ -18,20 +18,56 @@ documentService.save = async (req, res, next) => {
             for (const key in fileParser[parser]) {
                 newCnabDocument[key] = fileParser[parser][key].read(textLine);
             }
-            console.log(newCnabDocument);
             cnabDocuments.push(newCnabDocument);
         });
         const result = await CnabDocument.bulkCreate(cnabDocuments);
         res.status(200).json(result);
     } catch (error) {
-        console.error(error);
         res.status(500).json(error);
     }
 }
 
 documentService.getTransactionTypes = async (req, res, next) => {
-    const result = await TransactionType.findAll();
-    res.json(result);
+    try {
+        const result = await TransactionType.findAll();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+documentService.getCnabDocuments = async (req, res, next) => {
+    try {
+        const result = await CnabDocument.findAll();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+documentService.getGroupedDocument = async (req, res, next) => {
+    try {
+        const list = await CnabDocument.findAll({ attributes: ['shopname'], group: 'shopname' });
+        res.status(200).json(list);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+documentService.getCnabDocument = async (req, res, next) => {
+    try {
+        const { shopName } = req.body;
+        const list = await CnabDocument.findAll({ where: shopName });
+        const totalBalance = result.reduce((prev, current) => prev.value + current.value, 0)
+
+        const responseData = {
+            list,
+            totalBalance
+        }
+        res.status(200).json(responseData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 module.exports = documentService;
